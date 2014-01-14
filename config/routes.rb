39b7a8679088d1ee43aa8badbe0056df71b8e53a
Zack <zglick@amazon.com>
2014-01-14 02:27:51 +0000

    index and show views for missions are done.  Adding edit then giving access contorl a shot

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 RailsSr20x::Application.routes.draw do
   root :to => 'visitors#new'
+	resources :missions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114005929) do
+ActiveRecord::Schema.define(version: 20140114021627) do
 
   create_table "missions", force: true do |t|
     t.string   "name"
