2d3d7ad93912e1ecf9826638c8e0be6ef8fb147e
Zack <zglick@amazon.com>
2014-01-15 02:58:40 +0000

    basic auth in place for users.  going to attempt to port it over to missions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	
-	## added by https://tutorials.railsapps.org/tutorials/rails3-bootstrap-devise-cancan#authentication but 
-	## the turorial seemed to fall apart here
-	#validates_presence_of :name
-	#validates_uniqueness_of :name, :email, :case_sensitive => false
+	# Attempted to add the lines below but was warned by rails
+	# `attr_accessible` is extracted out of Rails into a gem. Please use new recommended protection model for params(strong_parameters) or add `protected_attributes` to your Gemfile to use old one.
+	# Setup accessible (or protected) attributes for your model
+	#attr_accessible :role_ids, :as => :admin
 	#attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+	before_create :assign_role
 
+  def assign_role
+    # assign a default role if no role is assigned
+    self.add_role :user if self.roles.first.nil?
+  end
 end
