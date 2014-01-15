53c08b78d4b3cb127450fc1ee8e0719b15d1d1b3
Zack <zglick@amazon.com>
2014-01-15 02:37:27 +0000

    users index updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-
-	def index
-    authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.all
-  end
+  load_and_authorize_resource :only => :index
 
   def show
     @user = User.find(params[:id])
