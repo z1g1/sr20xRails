d7ee3ba62209e388325723a9ea4d70ec6fe02d9c
Zack <zglick@amazon.com>
2014-01-17 23:46:21 +0000

    mission select on the front page complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140116232845) do
+ActiveRecord::Schema.define(:version => 201401762328452) do
 
   create_table "missions", :force => true do |t|
     t.string   "name"
