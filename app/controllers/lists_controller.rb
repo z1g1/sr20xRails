dc7c59736b0d227c79a7f0525a47b4b89c92ceb4
Zack <zglick@amazon.com>
2014-01-18 22:35:23 +0000

    list index convinced me to switch to tables, going back and modifying all views, while I'm there i will be adding links to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,8 @@ class ListsController < ApplicationController
 	end 
 
 	def create
+		@user = current_user
+		params[:list][:user_id]= @user.id 
 		@list = List.new(list_params)
 	  if @list.save
 			redirect_to @list
@@ -14,31 +16,40 @@ class ListsController < ApplicationController
 	end
 
 	def index
-		@lists = List.all.limit(30)
-		@factions = Faction.all
-		@casters = Caster.all.order(:title)
+		@lists = List.all
 	end
 
 	def show
 		@list = List.find(params[:id])
+		@faction = Faction.find(@list.faction_id)
+		@caster = Caster.find(@list.caster_id)
+		@author = User.find(@list.user_id)
 	end
 
 	def edit
 		@list = List.find(params[:id])
+		@faction = Faction.find(@list.faction_id)
+		@caster = Caster.find(@list.caster_id)
+		@author = User.find(@list.user_id)
 	end
 
 	def update
-		@list = Post.find(params[:id])
+		@list = List.find(params[:id])
  
-	  if @list.update(params[:list].permit(:title,:list_body,:size,:faction_id,:caster_id))
+	  if @list.update_attributes(params[:list].permit(:title,:body,:size,:faction_id,:caster_id,:user_id))
 		  redirect_to @list
 	  else
 	    render 'edit'
 	  end
 	end
-
+	def destroy
+		@list = List.find(params[:id])
+	  @list.destroy
+ 
+	  redirect_to lists_path
+	end
 	private
 		def list_params
-			params.require(:list).permit(:title,:list_body,:size,:faction_id,:caster_id)
+			params.require(:list).permit(:title,:body,:size,:faction_id,:caster_id,:user_id)
 	  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ActiveRecord::Base
   belongs_to :faction
   belongs_to :caster
   belongs_to :user
-  attr_accessible :body, :size, :title
+  attr_accessible :body, :size, :title, :faction_id, :caster_id, :user_id
 end
