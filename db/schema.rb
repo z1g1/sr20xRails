6a8f065d2c995d436a3485d42f5c53b0de0044de
z1g1 <mail@zackglick.com>
2014-01-22 20:57:46 +0000

    main page updated ready for intial publishing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140121233733) do
+ActiveRecord::Schema.define(:version => 201401212337332) do
 
   create_table "missions", :force => true do |t|
     t.string   "name"
